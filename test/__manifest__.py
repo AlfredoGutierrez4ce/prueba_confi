4be0aacc5254f2a1aded874224cb9af981ab8395
AlfredoGutierrez4ce <alfredo.gutierrez@4ce.com.mx>
2022-04-26 15:13:28 +0000

    [ADD] Basic Module

---
+++ test/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'sumary': """Academy app to manage Training""",
+    
+    'description': """
+       Academy Module to manage Traning
+       - Courses
+       - Sessions
+       - Attendees
+    """,
+    
+    'author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        #'security/academy_security.xml',
+        #'security/ir.model.access.csv',
+        #'views/academy_menuitems.xml',
+        #'views/course_views.xml',
+    ],
+    
+    'demo': [
+        #'demo/academy_demo.xml',
+        
+    ],
+}
